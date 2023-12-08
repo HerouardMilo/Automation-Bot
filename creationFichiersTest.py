a710885f5e45e8beb7627eb7899a11fe5586f2a7
Milo herouard <milo.herouard@gmail.com>
2023-12-08 20:32:37 +0100

    Script pour tester le programme sur des repertoires test

---
+++ creationFichiersTest.py
@@ -0,0 +1,18 @@
+import os
+
+if __name__ == "__main__":
+    image_extensions = ['.jpg', '.jpeg', '.png', '.gif']
+    document_extensions = ['.pdf', '.docx', '.xlsx', '.txt']
+    video_extensions = ['.mp4', '.avi', '.mkv']
+
+    all_extensions =image_extensions + document_extensions + video_extensions
+
+    for extension in all_extensions:
+        file_path =f'{extension}.{extension}'
+
+        try:
+            with open(file_path, 'w') as file:
+                pass
+            print(f'The file {file_path} has been created successfully')
+        except OSError as e:
+            print(f"Error opening: {file_path} - {e.strerror}")

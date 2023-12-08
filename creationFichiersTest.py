8c414663bf27e5391178ae4ac5d64cc2d3243eeb
Milo herouard <milo.herouard@gmail.com>
2023-12-08 20:50:33 +0100

    Les fichiers vont maintenant dans un répertoire et non pas à la racine

--- creationFichiersTest.py
+++ creationFichiersTest.py
@@ -5,10 +5,16 @@ if __name__ == "__main__":
     document_extensions = ['.pdf', '.docx', '.xlsx', '.txt']
     video_extensions = ['.mp4', '.avi', '.mkv']
 
+    base_directory = 'testOrigine'
+
+    if not os.path.exists(base_directory):
+        os.maledirs(base_directory)
+
     all_extensions =image_extensions + document_extensions + video_extensions
 
     for extension in all_extensions:
-        file_path =f'{extension}.{extension}'
+        file_name = f'sample.{extension}'
+        file_path = os.path.join(base_directory, file_name)
 
         try:
             with open(file_path, 'w') as file:
